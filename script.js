f0ee195d638d79f74c7bf7f8555d92957a835655
redwan <redwanthr@gmail.com>
2023-07-10 10:11:39 -0700

    Add Display function in Script.js

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ const books = [
   },
 ];
 
+
+// Display books
+function displayBooks() {
+  let book = '';
+  for (let i = 0; i < books.length; i += 1) {
+    book += `<p>${books[i].Title}</p>`;
+    book += `<p>${books[i].Author}</p>`;
+    book += `<button onclick="removeBook(${books[i].Id})">Remove</button><br>`;
+    book += '<hr>';
+  }
+  document.getElementById('display').innerHTML = book;
+}
+displayBooks();
+
+// Add books
+function addBook() {
+  const title = document.getElementById('Title').value;
+  const author = document.getElementById('Author').value;
+  if (title && author) {
+    const bookId = books.length + 1;
+    books.push({ Id: bookId, Title: title, Author: author });
+    displayBooks();
+    document.querySelector('form').reset();
+    localStorage.setItem('myBooklist', JSON.stringify(books));
+  }
+}
+addBook();
+
