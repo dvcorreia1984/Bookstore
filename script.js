b1c658c3dc4c3a950f710d6a431c54037b8d4699
dvcorreia1984 <victor@onsleer.co.za>
2023-07-10 18:58:33 +0200

    Added script.js file with book array

---
+++ script.js
@@ -0,0 +1,12 @@
+const books = [
+  {
+    Id: 1,
+    Title: 'Lorem ipsum',
+    Author: 'Testeroo Testyy',
+  },
+  {
+    Id: 2,
+    Title: 'Second Books',
+    Author: 'Testeroo Testyy',
+  },
+];
