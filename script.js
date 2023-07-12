c75620483a9566d3d5d89cf9407345c60d6bc961
redwan <redwanthr@gmail.com>
2023-07-12 03:16:04 -0700

    Update script.js and index.html

--- script.js
+++ script.js
@@ -87,6 +87,28 @@ class Books {
     document.getElementById('date').innerHTML = dateId;
   }
 
+  lists(){
+    document.getElementById('add-new').style.display='none';
+    document.getElementById('contactus').style.display='none';
+    document.getElementById('lists').style.display='flex';
+    document.getElementById('container').style.display='flex';
+  }
+
+  addNewBook(){
+    document.getElementById('lists').style.display='none';
+    document.getElementById('contactus').style.display='none';
+    document.getElementById('add-new').style.display='flex';
+    document.getElementById('container').style.display='flex';
+  }
+  contactus(){
+    document.getElementById('lists').style.display='none';
+    document.getElementById('contactus').style.display='flex';
+    document.getElementById('add-new').style.display='none';
+    document.getElementById('container').style.display='none';
+
+  }
+ 
+
 }
 
 // Create object of Books class
